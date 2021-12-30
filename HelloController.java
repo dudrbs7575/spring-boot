ed2bf5319d785d93318b212773a08205e8879b4a
syg <dudrbs7575@naver.com>
2021-12-30 18:54:16 +0900

    @ResponseBody으로 key값 전달

--- HelloController.java
+++ HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,11 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name" , name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 header부와 body부 존재. 응답 body부에 이 내용을 직접 넣어주는 것.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
     }
 }
